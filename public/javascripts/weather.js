974092cc45a82c24cef0e6bfd106846596ca99b2
kobayashi <kobayashi-keita@jreast.co.jp>
2025-09-26 15:41:54 +0900

    show wether at campground　revise

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -1,11 +1,11 @@
 async function getWeather(lat, lon) {
   const res = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max&models=jma_seamless&current=temperature_2m`
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max&models=jma_seamless&current_weather=true`
   );
   const data = await res.json();
 
   const weatherDiv = document.createElement("div");
-  weatherDiv.innerHTML = `<p>気温: ${data.current.temperature_2m}°C</p>`;
+  weatherDiv.innerHTML = `<p>気温: ${data.current_weather.temperature}°C</p>`;
   document.body.appendChild(weatherDiv);
 }
 
