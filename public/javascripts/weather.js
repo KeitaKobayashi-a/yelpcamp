877bbc20edd84fa65434e1b7af75fec453ef7e1e
kobayashi <kobayashi-keita@jreast.co.jp>
2025-09-26 17:54:10 +0900

    style change

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -1,13 +1,13 @@
 async function getWeather(lat, lon) {
   try {
     const res = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max&models=jma_seamless&current_weather=true`
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m&forecast_days=1`
     );
     const data = await res.json();
     console.log("APIレスポンス", data);
 
     const weatherDiv = document.getElementById("weather-info");
-    weatherDiv.textContent = `気温: ${data.current_weather.temperature}°C`;
+    weatherDiv.textContent = `気温: ${data.current.temperature_2m}°C`;
   } catch (error) {
     console.error("fetchエラー:", error);
   }
